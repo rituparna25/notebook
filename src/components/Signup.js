55c7eaf0556d1b47b47fd89f98800d54dbb062f5
Rituparna Biswas <145699840+rituparna25@users.noreply.github.com>
2025-03-14 23:36:34 +0530

    API endpoints changed acor to Backend url

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -7,7 +7,7 @@ const Signup = (props) => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const response = await fetch("http://localhost:5000/api/auth/createuser", {
+        const response = await fetch("https://inotebook-backend-wpn2.onrender.com/api/auth/createuser", {
           method: "POST",
           body: JSON.stringify({name: credentials.name, email: credentials.email, password: credentials.password,cpassword:credentials.cpassword }),
           headers: {
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import NoteContext from "./NoteContext";
 
 const NoteState = (props) => {
-  const host = "http://localhost:5000";
+  const host = "https://inotebook-backend-wpn2.onrender.com";;
   const notesInitial = [];
   const [notes, setNotes] = useState(notesInitial);
 
